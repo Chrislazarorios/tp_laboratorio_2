d6cca4ed0310f5204469bac780789ad0ab64ec7d
Cristian Lopez <chris-dmt@hotmail.com>
2019-11-05 01:06:19 -0300

    asd

---
+++ RecuperatoriosTP/TP-02/Entidades/Dulce.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    /// <summary>
+    /// La clase Dulces es publica.
+    /// </summary>
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string codigo, ConsoleColor color) : base(codigo, marca, color)
+        {
+
+        }
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+        /// <summary>
+        /// Publica todos los datos del Dulce.
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine((string)this);
+            sb.AppendLine($"CALORIAS : {this.CantidadCalorias}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
